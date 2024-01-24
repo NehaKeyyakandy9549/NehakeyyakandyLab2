0503bbbad3d0b603b9f7c664aa9065857eb9d364
NehaKeyyakandy9549 <n01579549@humber.ca>
2024-01-24 18:59:11 -0500

    The one with snackbar with action close

--- app/src/main/java/neha/keyyakandy/n01579549/MainActivity.java
+++ app/src/main/java/neha/keyyakandy/n01579549/MainActivity.java
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +35,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        View rootView = findViewById(android.R.id.content);
+        rootView.setFocusableInTouchMode(true);
+        rootView.requestFocus();
+        rootView.setOnKeyListener((v, keyCode, event) -> {
+            if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+                showSnackbar();
+                return true; // consume the event
+            }
+            return false;
+        });
     }
+
+    private void showSnackbar() {
+        Snackbar.make(findViewById(android.R.id.content),
+                        "Press 'Close' to exit", Snackbar.LENGTH_LONG)
+                .setAction("Close", v -> finish())
+                .show();
+
+
+
+    }
+
+
 }
